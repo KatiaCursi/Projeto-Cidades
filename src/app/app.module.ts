f83406fd163a7a72ac753e6829fc6638e9cab1a8
KatiaCursi <cursikatia@gmail.com>
2024-10-08 10:19:04 -0300

    criação do novo componente tabelaCidades

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TabelaCidadesComponent } from './tabela-cidades/tabela-cidades.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabelaCidadesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
